8ed884d914746559e50fe8b494618a0daa9329c9
jungdaehun1024 <pooh99102408@gmail.com>
2024-09-29 16:48:25 +0900

    프런트 로그인 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,14 +1,42 @@
 "use strict";
 
-const hello = (req, res) => {
-    res.render("home/index");
- };
+const output = {
+    home: (req, res) => {
+        res.render("home/index");
+     },
 
- const login = (req, res) => {
-    res.render("home/login");
+     login:(req, res) => {
+        res.render("home/login")
+    }
+}
+
+const users = {
+    id:["qwer123","정대훈","eogns"],
+    password:["1234","1234","12345"],
 };
 
+const process = {
+    login:(req,res)=>{
+        const id = req.body.id,
+            password=req.body.password;
+
+            if(users.id.includes(id)){
+                const idx = users.id.indexOf(id);
+                if(users.password[idx] ===password){
+                    return res.json({
+                        success: true,
+                    });
+                }
+            }
+
+            return res.json({
+                success: false,
+                msg:"로그인에 실패",
+            });
+    },
+}
+
 module.exports = {
-    hello,
-    login,
+    output,
+    process
 };
--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -5,9 +5,9 @@ const router = express.Router();
 
 const ctrl = require("./home.ctrl");
 
-router.get("/",ctrl.hello );
- 
-router.get("/login",ctrl.login);
+router.get("/",ctrl.output.home );
+router.get("/login",ctrl.output.login);
+router.post("/login",ctrl.process.login);
 
  //라우터 객체를 외부파일에서 사용할 수 있게 내보내기(exprots)
  module.exports = router;

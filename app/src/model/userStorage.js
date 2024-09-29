493a7d820df072e8a813941f83c8cb81c89c6502
jungdaehun1024 <pooh99102408@gmail.com>
2024-09-29 21:40:14 +0900

    로그인화면 꾸미기

--- app/src/model/userStorage.js
+++ app/src/model/userStorage.js
@@ -11,13 +11,24 @@ class UserStorage{
         const users =  this.#users;
         const newUsers = fields.reduce((newUsers,field)=>{
             if(users.hasOwnProperty(field)){
-                newUsers[field] = users[field];
+                newUsers[field] = users[field]; //newUsers에 대괄호 표기법 사용해 동적으로 속성 추가
             }
-
             return newUsers;
         },{});
         return newUsers ;
         
+    }
+
+    static getUserInfo(id){
+        const users = this.#users;
+        const idx = users.id.indexOf(id);
+        const userKeys = Object.keys(users); // users의 key값들만 배열로
+        const userInfo = userKeys.reduce((newUser, info)=>{
+            newUser[info] = users[info][idx];//users의 key값의 idx
+            return newUser;
+        },{});
+
+        return userInfo;
     }
 }
 

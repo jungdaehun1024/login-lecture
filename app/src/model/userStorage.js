474a035dc384960c19d14764b29ba442d96f66f8
jungdaehun1024 <pooh99102408@gmail.com>
2024-10-02 00:26:54 +0900

    fs모듈을 통한 로그인 기능 구현

--- app/src/model/userStorage.js
+++ app/src/model/userStorage.js
@@ -1,14 +1,21 @@
 "use strict"
-
+const fs = require("fs").promises;
 class UserStorage{
-   static #users = {
-        id:["qwer123","정대훈","eogns"],
-        password:["1234","1234","12345"],
-        name:["정별이","별이","김별이"]
-    };
+    static #getUserInfo(data,id){
+        const users = JSON.parse(data);
+        const idx = users.id.indexOf(id);
+        const userKeys = Object.keys(users); // users의 key값들만 배열로
+        const userInfo = userKeys.reduce((newUser, info)=>{
+            newUser[info] = users[info][idx];//users의 key값의 idx
+            return newUser;
+        },{});
+
+        return userInfo;
+    }
+ 
 
     static getUsers(...fields){
-        const users =  this.#users;
+        // const users =  this.#users;
         const newUsers = fields.reduce((newUsers,field)=>{
             if(users.hasOwnProperty(field)){
                 newUsers[field] = users[field]; //newUsers에 대괄호 표기법 사용해 동적으로 속성 추가
@@ -20,20 +27,20 @@ class UserStorage{
     }
 
     static getUserInfo(id){
-        const users = this.#users;
-        const idx = users.id.indexOf(id);
-        const userKeys = Object.keys(users); // users의 key값들만 배열로
-        const userInfo = userKeys.reduce((newUser, info)=>{
-            newUser[info] = users[info][idx];//users의 key값의 idx
-            return newUser;
-        },{});
-
-        return userInfo;
+      
+    return fs.readFile("./src/databases/users.json")
+    .then((data)=>{
+        return this.#getUserInfo(data,id);
+    })
+    .catch((err)=> console.error(err));
+       
     }
+
+
 
     static save(userInfo){
         console.log(userInfo.id);
-        const users = this.#users;
+        // const users = this.#users;
         users.id.push(userInfo.id);
         users.name.push(userInfo.name);
         users.password.push(userInfo.password);
